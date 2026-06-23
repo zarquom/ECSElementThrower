dffb9454eeb6c9e6321e8a01a5810b2219ba03f8
Marcos Paulis <moonspell.doom@gmail.com>
2026-06-23 09:00:13 +0200

    Add placeholders for more elements and how to represent them

--- Assets/Scripts/UI/Collectibles/CollectiblesView.cs
+++ Assets/Scripts/UI/Collectibles/CollectiblesView.cs
@@ -9,11 +9,16 @@ public class CollectiblesView : MonoBehaviour
 {
     private CollectiblesModel _pointsModel;
 
+    private void Start()
+    {
+        _pointsModel.PointsText.text = "<sprite name=Fire>: 0 <sprite name=Water>: 0 <sprite name=Earth>: 0 <sprite name=Wind>: 0";
+    }
+
     public void UpdateView(DynamicBuffer<CollectibleElement> collectibles)
     {
         if(collectibles.IsEmpty)
         {
-            _pointsModel.PointsText.text = "Fire: 0, Water: 0, Earth: 0, Wind: 0";
+            _pointsModel.PointsText.text = "<sprite name=Fire>: 0 <sprite name=Water>: 0 <sprite name=Earth>: 0 <sprite name=Wind>: 0";
             return;
         } else
         {
@@ -30,15 +35,15 @@ public class CollectiblesView : MonoBehaviour
             var element = buffer[i];
 
             if (i > 0)
-                result.Append(new FixedString32Bytes(", "));
+                result.Append(new FixedString32Bytes(" "));
 
             // Append the enum name
             switch (element.Type)
             {
-                case CollectibleType.Fire: result.Append(new FixedString32Bytes("Fire")); break;
-                case CollectibleType.Water: result.Append(new FixedString32Bytes("Water")); break;
-                case CollectibleType.Earth: result.Append(new FixedString32Bytes("Earth")); break;
-                case CollectibleType.Wind: result.Append(new FixedString32Bytes("Air")); break;
+                case CollectibleType.Fire: result.Append(new FixedString32Bytes("<sprite name=Fire>")); break;
+                case CollectibleType.Water: result.Append(new FixedString32Bytes("<sprite name=Water>")); break;
+                case CollectibleType.Earth: result.Append(new FixedString32Bytes("<sprite name=Earth>")); break;
+                case CollectibleType.Wind: result.Append(new FixedString32Bytes("<sprite name=Wind>")); break;
             }
 
             result.Append(new FixedString32Bytes(": "));
